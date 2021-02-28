001304fe9cc53b9618b7c1c90347acefdc896f0c
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-02-28 16:19:28 +0200

    task2

---
+++ app/src/main/java/com/menu/task2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.menu.task2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void createNewGroup(View view) {
+        Toast.makeText(this, "قمت بالضغط على إنشاء مجموعة جديدة", Toast.LENGTH_LONG).show();
+    }
+}
