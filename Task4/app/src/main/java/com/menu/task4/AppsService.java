57815b76a56cf74e1e4d953c605b1bd0be332dfc
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-03-24 22:20:12 +0200

    task4

--- Task4/app/src/main/java/com/menu/task4/AppsService.java
+++ Task4/app/src/main/java/com/menu/task4/AppsService.java
@@ -1,91 +1,172 @@
 package com.menu.task4;
 
+
 import android.app.ActivityManager;
 import android.app.Service;
-import android.app.usage.UsageEvents;
-import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Binder;
-import android.os.Build;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class AppsService extends Service {
-    private final IBinder binder = new LocalBinder();
-    // Random number generator
-    private final Random mGenerator = new Random();
-
-    public AppsService() {
-    }
+    public static final String TAG = "AppsService";
+    private Context context = null;
+    SharedPreferences sharedPreference;
+    SharedPreferences.Editor editor;
+    List<String> stalkList;
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
+    public void onCreate() {
+        super.onCreate();
+        stalkList = new ArrayList<>();
+        context = getApplicationContext();
+        sharedPreference = getSharedPreferences(context.getString(R.string.preference_file_key), MODE_PRIVATE); /* @prmay*/
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+                final List<ActivityManager.RunningTaskInfo> services = activityManager.getRunningTasks(Integer.MAX_VALUE);
+                for (int i = 0; i < services.size(); i++) {
+                    if (!stalkList.contains(services.get(i).baseActivity.getPackageName())) {
+                        stalkList.add(services.get(i).baseActivity.getPackageName());
+                    }
+                }
+
+                List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+                for (int i = 0; i < procInfos.size(); i++) {
+
+                    ArrayList<String> runningPkgs = new ArrayList<String>(Arrays.asList(procInfos.get(i).pkgList));
+
+                    Collection diff = subtractSets(runningPkgs, stalkList);
+
+                    if (diff != null) {
+                        stalkList.removeAll(diff);
+                    }
+                }
+
+
+            }
+        }, 20000, 6000);  // every 6 seconds
+
+
         return START_STICKY;
     }
 
-    public class LocalBinder extends Binder {
-        AppsService getService() {
-            // Return this instance of LocalService so clients can call public methods
-            return AppsService.this;
-        }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
-    UsageStatsManager usageStatsManager;
 
-    public int getRandomNumber(int number) {
-        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            usageStatsManager = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            List<ActivityManager.RunningTaskInfo> runningTaskInfos = activityManager.getRunningTasks(1);
-            if (runningTaskInfos != null && !runningTaskInfos.isEmpty()) {
-                System.out.println(runningTaskInfos.get(0).topActivity.getPackageName());
+    private ActivityManager.RunningAppProcessInfo getForegroundApp() {
+        ActivityManager.RunningAppProcessInfo result = null, info = null;
 
-            } else {
-                long endTime = System.currentTimeMillis();
-                long beginTime = 10000;
-                String result = "";
-                UsageEvents.Event event = new UsageEvents.Event();
-                UsageEvents usageEvents = usageStatsManager.queryEvents(beginTime, endTime);
-                while (usageEvents.hasNextEvent()) {
-                    usageEvents.getNextEvent(event);
-                    if (event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND && !event.getPackageName().equals(getPackageName())) {
-                        System.out.println("package : " + event.getPackageName());
-                        return number++;
-                    }
-                }
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningAppProcessInfo> l = activityManager.getRunningAppProcesses();
+        Iterator<ActivityManager.RunningAppProcessInfo> i = l.iterator();
+        while (i.hasNext()) {
+            info = i.next();
+            if (info.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+                    && !isRunningService(info.processName)) {
+                result = info;
+                break;
             }
         }
-        return 0;
+        return result;
     }
 
-    public String getForegroundApp(Context context) {
-        ActivityManager am = (ActivityManager) context.getSystemService(Service.ACTIVITY_SERVICE);
-        ActivityManager.RunningTaskInfo foregroundTaskInfo = am.getRunningTasks(1).get(0);
-        String foregroundTaskPackageName = foregroundTaskInfo.topActivity.getPackageName();
-        PackageManager pm = context.getPackageManager();
-        PackageInfo foregroundAppPackageInfo = null;
-        try {
-            foregroundAppPackageInfo = pm.getPackageInfo(foregroundTaskPackageName, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            return null;
+    private boolean isRunningService(String processName) {
+        if (processName == null)
+            return false;
+
+        ActivityManager.RunningServiceInfo service;
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningServiceInfo> l = activityManager.getRunningServices(9999);
+        Iterator<ActivityManager.RunningServiceInfo> i = l.iterator();
+        while (i.hasNext()) {
+            service = i.next();
+            if (service.process.equals(processName))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isRunningApp(String processName) {
+        if (processName == null)
+            return false;
+
+        ActivityManager.RunningAppProcessInfo app;
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningAppProcessInfo> l = activityManager.getRunningAppProcesses();
+        Iterator<ActivityManager.RunningAppProcessInfo> i = l.iterator();
+        while (i.hasNext()) {
+            app = i.next();
+            if (app.processName.equals(processName) && app.importance != ActivityManager.RunningAppProcessInfo.IMPORTANCE_SERVICE)
+                return true;
+        }
+        return false;
+    }
+
+
+    private boolean checkifThisIsActive(ActivityManager.RunningAppProcessInfo target) {
+        boolean result = false;
+        ActivityManager.RunningTaskInfo info;
+
+        if (target == null)
+            return false;
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningTaskInfo> l = activityManager.getRunningTasks(9999);
+        Iterator<ActivityManager.RunningTaskInfo> i = l.iterator();
+
+        while (i.hasNext()) {
+            info = i.next();
+            if (info.baseActivity.getPackageName().equals(target.processName)) {
+                result = true;
+                break;
+            }
         }
 
-        String foregroundApp = null;
-        if (foregroundAppPackageInfo != null)
-            foregroundApp = foregroundAppPackageInfo.applicationInfo.packageName;
-        return foregroundApp;
+        return result;
+    }
+
+
+    // what is in b that is not in a ?
+    public static Collection subtractSets(Collection a, Collection b) {
+        Collection result = new ArrayList(b);
+        result.removeAll(a);
+        return result;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        int numbers = sharedPreference.getInt(context.getString(R.string.number_run_other_apps), 0);
+        editor = sharedPreference.edit();
+        editor.putInt(context.getString(R.string.number_run_other_apps), numbers + stalkList.size());
+        editor.apply();
+        editor.commit();
     }
 }
--- Task4/app/src/main/java/com/menu/task4/MainActivity.java
+++ Task4/app/src/main/java/com/menu/task4/MainActivity.java
@@ -1,16 +1,13 @@
 package com.menu.task4;
 
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private int numberOnBackPressInt;
     private int numberRunOtherAppsInt;
     private SharedPreferences.Editor editor;
-    private AppsService mService;
-    private boolean mBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         numberRunOtherAppsInt = sharedPref.getInt(getString(R.string.number_run_other_apps), 0);
 
         if (numberStartAppInt == 0) {
-            Toast.makeText(this, "مرحبًا بك في تطبيق Task 4 ", Toast.LENGTH_SHORT).show();
+            alertDialog("رسالة ترحيبية", "مرحبًا بك في تطبيق Task 4😍😍");
         } else {
-            Toast.makeText(this, "أهلًا بعودتك", Toast.LENGTH_SHORT).show();
+            alertDialog("نرحب بك مرة اخرى 🤩", "أهلًا بعودتك 😊");
         }
         numberStartAppInt++;
         editor = sharedPref.edit();
@@ -56,38 +51,28 @@ public class MainActivity extends AppCompatActivity {
         numberOnBackPressInt++;
         editor.putInt(getString(R.string.number_on_back_press), numberOnBackPressInt);
         editor.apply();
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, AppsService.class);
-        bindService(intent, connection, BIND_AUTO_CREATE);
+        stopService(new Intent(this, AppsService.class));
+        numberRunOtherAppsInt = sharedPref.getInt(getString(R.string.number_run_other_apps), 0);
+        numberRunOtherApps.setText(numberRunOtherAppsInt + "");
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
     @Override
     protected void onStop() {
         super.onStop();
-        if (mBound) {
-            numberRunOtherAppsInt = sharedPref.getInt(getString(R.string.number_run_other_apps), 0);
-            String s = mService.getForegroundApp(this);
-            if (!s.equals(getPackageName())) {
-                numberRunOtherAppsInt++;
-            }
-            editor.putInt(getString(R.string.number_run_other_apps), numberRunOtherAppsInt);
-            editor.apply();
-        }
+        startService(new Intent(this, AppsService.class));
     }
+
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        numberRunOtherAppsInt = sharedPref.getInt(getString(R.string.number_run_other_apps), 0);
-        numberRunOtherApps.setText(numberRunOtherAppsInt + "");
-        unbindService(connection);
-        mBound = false;
+
     }
 
     @Override
@@ -95,20 +80,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private final ServiceConnection connection = new ServiceConnection() {
 
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            AppsService.LocalBinder binder = (AppsService.LocalBinder) service;
-            mService = binder.getService();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
+    private void alertDialog(String title, String message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .show();
+    }
 }
