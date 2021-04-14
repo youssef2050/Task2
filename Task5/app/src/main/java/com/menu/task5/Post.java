241265b0bfa5310870e94eac5bb31e766c777e7f
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-04-14 06:30:19 +0300

    Adapter

---
+++ Task5/app/src/main/java/com/menu/task5/Post.java
@@ -0,0 +1,87 @@
+package com.menu.task5;
+
+public class Post {
+    private long id;
+    private String name;
+    private String body;
+    private String date;
+    private long followers;
+    private long following;
+    private long posts;
+    private int image;
+
+    public Post(long id, String name, String body, String date, long followers, long following, long posts, int image) {
+        this.id = id;
+        this.name = name;
+        this.body = body;
+        this.date = date;
+        this.followers = followers;
+        this.following = following;
+        this.posts = posts;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(long followers) {
+        this.followers = followers;
+    }
+
+    public long getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(long following) {
+        this.following = following;
+    }
+
+    public long getPosts() {
+        return posts;
+    }
+
+    public void setPosts(long posts) {
+        this.posts = posts;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ Task5/app/src/main/java/com/menu/task5/PostAdapter.java
@@ -0,0 +1,47 @@
+package com.menu.task5;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class PostAdapter extends BaseAdapter {
+    private List<Post> posts;
+    private Context context;
+
+    public PostAdapter(List<Post> posts, Context context) {
+        this.posts = posts;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return posts.size();
+    }
+
+    @Override
+    public Post getItem(int position) {
+        return posts.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return posts.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.post_item, parent, false);
+        }
+        //TODO inflate for items in post_item
+
+        //TODO get Post and create result in items
+        Post post = getItem(position);
+        return view;
+    }
+}
