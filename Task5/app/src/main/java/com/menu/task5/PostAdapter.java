4e266677f581311e3aad7c8efc67a77359795b36
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-04-17 05:27:38 +0300

    adapter is done!

--- Task5/app/src/main/java/com/menu/task5/PostAdapter.java
+++ Task5/app/src/main/java/com/menu/task5/PostAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,11 +47,25 @@ public class PostAdapter extends BaseAdapter {
             view = LayoutInflater.from(context).inflate(R.layout.post_item, parent, false);
         }
         //TODO inflate for items in post_item
+        TextView postFollowing = view.findViewById(R.id.post_following);
+        TextView postFollowers = view.findViewById(R.id.post_followers);
+        TextView posts = view.findViewById(R.id.post_num);
+        TextView postName = view.findViewById(R.id.post_name);
+        TextView postBody = view.findViewById(R.id.post_body);
+        TextView postDate = view.findViewById(R.id.post_date);
+        ImageView postImage = view.findViewById(R.id.post_img);
 
         //TODO get Post and create result in items
         Post post = getItem(position);
+        postFollowing.setText(post.getFollowing() + "");
+        postFollowers.setText(post.getFollowers() + "");
+        posts.setText(post.getPosts() + "");
+        postName.setText(post.getName());
+        postBody.setText(post.getBody());
+        postDate.setText(post.getDate());
+        postImage.setImageDrawable(context.getResources().getDrawable(post.getImage()));
 
-
+        //TODO return View object
         return view;
     }
 }
